30df86870e7ea593257d24ccc12ee6b6bc3ef327
BIURacing <racing.biu@biu.ac.il>
2026-07-31 17:40:51 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+export default defineConfig({
+  plugins: [react()],
+  base: '/BIU_Racing_Website/',
+})
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
